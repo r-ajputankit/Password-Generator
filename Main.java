7cdda7e8caceee6ebd202a5d1272d18a4d3d3977
Ankit <rajputankit2664@gmail.com>
2023-07-09 12:23:22 +0530

    added - include characters given by user

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Random;
 
 class Main {
     Random random = new Random();
+    Scanner sc = new Scanner(System.in);
 
     int includeInt() {
         int p = random.nextInt(10);
@@ -17,33 +18,111 @@ class Main {
         return (char) (random.nextInt(10) + 97);
     }
 
+    String charOfChoice(int m) {
+        // m is the length of the string to be include.
+        String pass = "";
+        System.out.print("\nEnter the length of password: ");
+        int num = sc.nextInt();
+
+        System.out.print("do you want to give character choice(y/n): ");
+        char ch = sc.next().charAt(0);
+        if (ch == 'y' || ch == 'Y') {
+            System.out.print("No of characters: ");
+            int n = sc.nextInt();
+            System.out.print("Enter the Array of characters: ");
+            char[] characters = new char[n];
+            // The array store the characters to be include in the password. Input by user.
+
+            for (int i = 0; i < n; i++) {
+                characters[i] = sc.next().charAt(0);
+            }
+            for (int i = 0; i < num-m; i++) {
+                pass += fromArray(characters, n);
+            }
+            return pass;
+        } else {
+            return generatePassword(num, m);
+            // num--> length of password
+            // m ---> length of the string
+        }
+    }
+
+    char fromArray(char[] arr, int n) {
+        int i = random.nextInt(n);
+        return arr[i];
+    }
+
+    String generatePassword(int n, int m) {
+        String pass = "";
+        for (int i = 0; i < n - m; i++) {
+            int a = random.nextInt(2);
+            if (a == 0) {
+                pass += includeInt();
+            } else {
+                pass += includeChar();
+            }
+        }
+        return pass;
+    }
+
     public static void main(String[] args) throws InterruptedException {
 
+        System.out.println("\n_______We are about to geberate a strong password_______\n");
         Scanner sc = new Scanner(System.in);
 
         Random random = new Random();
         Main ob = new Main();
         String password = "";
 
-        System.out.println();
-        System.out.print("Enter the length of the password:(Suggested 10): ");
-        int n = sc.nextInt();
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Including a string can be a Hint to recover password!!");
+        System.out.println("--------------------------------------------------------");
 
-        System.out.print("Do you want to include a starting String (y/n)");
+        System.out.print("Do you want to include a String? (y/n): ");
         char choice = sc.next().charAt(0);
+        System.out.println("-------------------------------------------");
+
         if (choice == 'y' || choice == 'Y') {
-            System.out.print("Enter the string: ");
+            System.out.print("\nEnter the string: ");
             String string = sc.next();
-            password = string;
-            for (int i = 0; i < n - (string.length()); i++) {
-                String s = Integer.toString(ob.includeInt());
-                password += s;
+            int m = string.length();
+
+            System.out.println("\n--------------------------");
+            System.out.println("Include String --> ");
+            System.out.println("--------------------------");
+            System.out.println("1. Begin with String\n2. End with String\n3. Do not include");
+            System.out.println("----------------------------------------------");
+
+            System.out.print("Choice: ");
+            int choic = sc.nextInt();
+
+            switch (choic) {
+                case 1:
+                    password = string + ob.charOfChoice(m);
+                    break;
+
+                case 2:
+                    password = ob.charOfChoice(m) + string;
+                    break;
+
+                case 3:
+                    System.out.println("Enter the length of password: ");
+                    int num = sc.nextInt();
+                    password += ob.generatePassword(num, 0);
+                    break;
+
+                default:
+                    System.out.println("Invalid Input !!! PROGRAM TERMINATED!!!!\n\n");
+                    sc.close();
+                    return;
             }
         } else {
+            System.out.print("Enter the length of the password: ");
+            int num = sc.nextInt();
             System.out.println("Generating a Random Password........");
             Thread.sleep(1000);
             password += ob.includeChar();
-            for (int i = 0; i < n - 1; i++) {
+            for (int i = 0; i < num - 1; i++) {
                 int m = random.nextInt(3);
                 if (m == 0) {
                     password += ob.includeInt();
@@ -52,8 +131,11 @@ class Main {
                 }
             }
         }
+        sc.close();
+
         System.out.println("----------------------------------");
         System.out.println("Generated password---> " + password);
         System.out.println("----------------------------------");
+
     }
 }
